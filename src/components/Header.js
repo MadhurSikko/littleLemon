6fa52a73a8de6d40d5714906935f095f467aadd9
MadhurSikko <madhur.dagar.s@gmail.com>
2023-10-18 03:02:35 +0530

    added semantic html

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+
+function Header() {
+    return (
+        <header>
+            {/*<img src="" alt="logo"></img>*/}
+            <Nav />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,33 @@
+function Main() {
+    return (
+        <main>
+            <article>
+                <h2>Little Lemon</h2>
+                <h3>Chicago</h3>
+                <p>We are a family owned <br /> Mediterranean resturant, <br /> focused on traditional <br /> recipies served with a modern <br /> twist.</p>
+                <button>Reserve a table</button>
+            </article>
+
+            <article>
+                <h1>This Weeks Specials</h1>
+                <button>Online Menus</button>
+            </article>
+            
+            <article>
+                <section>
+                    <b>Greek Salad</b>
+                </section>
+
+                <section>
+                    <b>Bruchetta</b>
+                </section>
+
+                <section>
+                    <b>Lemon Dessert</b>
+                </section>
+            </article>
+        </main>
+    )
+}
+
+export default Main;
